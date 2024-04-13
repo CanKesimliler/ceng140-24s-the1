522de7b568b721fe0bed8217c5143faf98b82a7e
Can <kesimliler123@gmail.com>
2024-04-13 21:06:29 +0000

    version 0.4

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # ceng140-24s-the1
 Take home exam 1 for the CENG140 course
-There are some limitations that prevent me from using libraries and pointer usage.  
+There are some limitations that prevent me from using libraries and pointers.  
 Also I have to use ANSI C so I did not have the access to some modern features like dynamic arrays.  
-All things considered I think memory side has a room for improvement with a better map and dynamic arrays but I think,
-it is pretty fast and has complexity of O(N^2) which may even be further reduced by implementing a kmp like algorithm to at the findExon2 function.
-
+All things considered I think memory side has a room for improvement with a better map and dynamic arrays.
 # Setbacks:
-- 1- Erroneous inputs are not taken into the consideration.  
-- 2- Intron Exon format is set.  
-- 3- Some edge cases might go under the radar and needs to be further investigated. 
-- 4- Since it was written according to the context of a given task, aminoacid table does not match with the real life and needs to be modified in case of real life usage.
+- 1- Intron Exon format is set.  
+- 2- Most of the edge cases are taken into consideration but rare inputs might go under the radar and needs to be further investigated. 
+- 3- Since it was written according to the context of a given task, aminoacid table does not match with the real life and needs to be modified in case of real life usage.
--- the1.c
+++ the1.c
@@ -2,43 +2,45 @@
 #define MAX_DNA_LENGTH 3005
 #define MAX_PROTEIN_LENGTH 3000
 
+const char aminoacids[21][6][3] = {
+    {"CCT", "CCG", "CCA", "CCC", "", ""},       /*gly 0*/
+    {"CGT", "CGA", "CGC", "CGG", "", ""},       /*ala 1*/
+    {"CAC", "CAT", "CAG", "CAA", "", ""},       /*val 2*/
+    {"TAA", "TAG", "TAT", "", "", ""},          /*ile 3*/
+    {"AAT", "AAC", "GAG", "GAC", "GAA", "GAT"}, /*leu 4*/
+    {"TCA", "TCG", "AGA", "AGG", "AGC", "AGT"}, /*ser 5*/
+    {"TGA", "TGG", "TGT", "TGC", "", ""},       /*thr 6*/
+    {"GGA", "GGG", "GGT", "GGC", "", ""},       /*pro 7*/
+    {"CTA", "CTG", "", "", "", ""},             /*asp 8*/
+    {"CTT", "CTC", "", "", "", ""},             /*glu 9*/
+    {"TTT", "TTC", "", "", "", ""},             /*lys 10*/
+    {"TCT", "TCC", "GCA", "GCG", "GCT", "GCC"}, /*arg 11*/
+    {"TTA", "TTG", "", "", "", ""},             /*asn*/
+    {"GTT", "GTC", "", "", "", ""},             /*gln*/
+    {"ACA", "ACG", "", "", "", ""},             /*cys*/
+    {"TAC", "", "", "", "", ""},                /*met*/
+    {"ACC", "", "", "", "", ""},                /*trp*/
+    {"AAA", "AAG", "", "", "", ""},             /*phe*/
+    {"ATA", "ATG", "", "", "", ""},             /*tyr*/
+    {"GTA", "GTG", "", "", "", ""},             /*his*/
+    {"ATT", "ATC", "ACT", "", "", ""}};         /*stop*/
 void getInput(char *, char *, int *, int *);
-int CheckCode(int key, char (*aminoacids)[6][3], char *readingFrame);
-int findExon1(char *, char *, char (*aminoacids)[6][3], int *, int *, int *);
-int findExon2(char *, char *, char (*aminoacids)[6][3], int *, int, int , int, int);
-int findExon3(char *, char *, char (*aminoacids)[6][3], int *, int *, int *);
+int CheckCode(int key, char *readingFrame);
+int findExon2(char *, char *, int *, int, int, int, int);
+int findExon3(char *, char *, int *, int *, int *);
 
-int main()
+int main(void)
 {
     int map[200];
-    char aminoacids[21][6][3] = {
-        {"CCT", "CCG", "CCA", "CCC", "", ""},       /*gly 0*/
-        {"CGT", "CGA", "CGC", "CGG", "", ""},       /*ala 1*/
-        {"CAC", "CAT", "CAG", "CAA", "", ""},       /*val 2*/
-        {"TAA", "TAG", "TAT", "", "", ""},          /*ile 3*/
-        {"AAT", "AAC", "GAG", "GAC", "GAA", "GAT"}, /*leu 4*/
-        {"TCA", "TCG", "AGA", "AGG", "AGC", "AGT"}, /*ser 5*/
-        {"TGA", "TGG", "TGT", "TGC", "", ""},       /*thr 6*/
-        {"GGA", "GGG", "GGT", "GGC", "", ""},       /*pro 7*/
-        {"CTA", "CTG", "", "", "", ""},             /*asp 8*/
-        {"CTT", "CTC", "", "", "", ""},             /*glu 9*/
-        {"TTT", "TTC", "", "", "", ""},             /*lys 10*/
-        {"TCT", "TCC", "GCA", "GCG", "GCT", "GCC"}, /*arg 11*/
-        {"TTA", "TTG", "", "", "", ""},             /*asn*/
-        {"GTT", "GTC", "", "", "", ""},             /*gln*/
-        {"ACA", "ACG", "", "", "", ""},             /*cys*/
-        {"TAC", "", "", "", "", ""},                /*met*/
-        {"ACC", "", "", "", "", ""},                /*trp*/
-        {"AAA", "AAG", "", "", "", ""},             /*phe*/
-        {"ATA", "ATG", "", "", "", ""},             /*tyr*/
-        {"GTA", "GTG", "", "", "", ""},             /*his*/
-        {"ATT", "ATC", "ACT", "", "", ""}};         /*stop*/
     int intrn1Start, intrn2Start, intrn1End, intrn2End;
     char dnaSequence[MAX_DNA_LENGTH] = "";
     char proteinSequence[MAX_PROTEIN_LENGTH] = "";
     int proteinLen = -1;
     int dnaLen = -1;
     int ldna, rdna, lpro, rpro;
+    char readingFrame[4];
+    int check = 1;
+    int k, l;
     intrn1Start = intrn1End = intrn2Start = intrn2End = 0;
     ldna = lpro = 0;
     map[(int)'g'] = 0;
@@ -64,35 +66,145 @@ int main()
     map[(int)'.'] = 20;
 
     getInput(dnaSequence, proteinSequence, &dnaLen, &proteinLen);
-    rdna = dnaLen - 4;
+    rdna = dnaLen - 3;
     rpro = proteinLen - 1;
-    if (!findExon1(dnaSequence, proteinSequence, aminoacids, map, &ldna, &lpro))
-    {
-        printf("NONE");
-        return 0;
-    }
-    intrn1Start = ldna;
-    if (!findExon3(dnaSequence, proteinSequence, aminoacids, map, &rdna, &rpro))
-    {
-        printf("NONE");
-        return 0;
-    }
-    intrn2End = rdna + 2;
-    if(!(intrn1End = findExon2(dnaSequence, proteinSequence, aminoacids, map, ldna, lpro, rdna, rpro)-1)){
-        printf("NONE");
-        return 0;
-    };
-    intrn2Start = intrn1End + 1 + (rpro-lpro+1)*3;
-    printf("\n%d %d %d %d", intrn1Start, intrn1End, intrn2Start, intrn2End);
-    fflush(NULL);
 
-    return 0;
+    /*Checking stop codon*/
+    readingFrame[0] = dnaSequence[rdna];
+    readingFrame[1] = dnaSequence[rdna + 1];
+    readingFrame[2] = dnaSequence[rdna + 2];
+    readingFrame[3] = '\0';
+    if (!CheckCode(map['.'], readingFrame))
+    {
+        printf("NONE");
+        return 0;
+    }
+
+    if (findExon3(dnaSequence, proteinSequence, map, &rdna, &rpro) == -1)
+    {
+        if(rpro < 0)
+        {
+            rpro += 2;
+            rdna += 5;
+        }
+    }
+    intrn2End = rdna;
+
+    /*Check First Codon*/
+    readingFrame[0] = dnaSequence[ldna];
+    readingFrame[1] = dnaSequence[ldna + 1];
+    readingFrame[2] = dnaSequence[ldna + 2];
+    readingFrame[3] = '\0';
+    if (CheckCode(map[(int)proteinSequence[lpro]], readingFrame))
+    {
+        ldna += 3;
+        lpro++;
+    }
+    else
+    {
+        printf("NONE");
+        return 0;
+    }
+
+    while (1)
+    {
+        readingFrame[0] = dnaSequence[ldna];
+        readingFrame[1] = dnaSequence[ldna + 1];
+        readingFrame[2] = dnaSequence[ldna + 2];
+        readingFrame[3] = '\0';
+        if (CheckCode(map[(int)proteinSequence[lpro]], readingFrame) || check)
+        {
+            if (!CheckCode(map[(int)proteinSequence[lpro]], readingFrame))
+            {
+                check = 0;
+            }
+            if (findExon2(dnaSequence, proteinSequence, map, ldna + 1, lpro, rdna, rpro) != -1)
+            {
+                intrn1Start = ldna;
+                intrn1End = map[1];
+                intrn2Start = map[0];
+                printf("%d %d %d %d", intrn1Start, intrn1End, intrn2Start, intrn2End);
+                fflush(NULL);
+                if (intrn1Start > intrn1End || intrn2Start > intrn2End || intrn1Start > intrn2Start || intrn1End > intrn2End)
+                {
+                    printf("NONE");
+                    fflush(NULL);
+                    return 0;
+                }
+                return 0;
+            }
+            else
+            {
+                k = rpro;
+                l = rdna;
+                while (k < proteinLen)
+                {
+                    k++;
+                    l += 3;
+                    if (findExon2(dnaSequence, proteinSequence, map, ldna + 1, lpro, l, k) != -1)
+                    {
+                        intrn1Start = ldna;
+                        intrn1End = map[1];
+                        intrn2Start = map[0];
+                        intrn2End = l;
+                        if (intrn1Start > intrn1End || intrn2Start > intrn2End || intrn1Start > intrn2Start || intrn1End > intrn2End)
+                        {
+                            printf("NONE");
+                            fflush(NULL);
+                            return 0;
+                        }
+                        printf("%d %d %d %d", intrn1Start, intrn1End, intrn2Start, intrn2End);
+                        fflush(NULL);
+                        return 0;
+                    }
+                }
+            }
+            lpro++;
+            ldna += 3;
+        }
+        else
+        {
+            printf("NONE");
+            fflush(NULL);
+            return 0;
+        }
+    }
 }
 
-int CheckCode(int key, char (*aminoacids)[6][3], char *readingFrame)
+int findExon2(char *dnaSequence, char *proteinSequence, int *map, int ldna, int lpro, int rdna, int rpro)
+{
+    char readingFrame[4] = "";
+    int i;
+    int k, l;
+    for (; ldna + 3 * (rpro - lpro) < rdna; ldna++)
+    {
+        i = ldna;
+        k = lpro;
+        l = rpro;
+        for (; k <= l; k++)
+        {
+            readingFrame[0] = dnaSequence[i];
+            readingFrame[1] = dnaSequence[i + 1];
+            readingFrame[2] = dnaSequence[i + 2];
+            readingFrame[3] = '\0';
+            if (!CheckCode(map[(int)proteinSequence[k]], readingFrame))
+                break;
+            i += 3;
+        }
+        if (k > l && lpro <= rpro)
+        {
+            map[0] = i;
+            map[1] = ldna - 1;
+            return ldna - 1;
+        }
+    }
+    return -1;
+}
+
+int CheckCode(int key, char *readingFrame)
 {
     int index = 0;
-    while (aminoacids[key][index][0] != '\0')
+    while (aminoacids[key][index][0] != '\0' && index < 6)
     {
         if (aminoacids[key][index][0] == readingFrame[0] &&
             aminoacids[key][index][1] == readingFrame[1] &&
@@ -105,77 +217,23 @@ int CheckCode(int key, char (*aminoacids)[6][3], char *readingFrame)
     return 0;
 }
 
-int findExon1(char *dnaSequence, char *proteinSequence, char (*aminoacids)[6][3], int *map, int *ldna, int *lpro)
+int findExon3(char *dnaSequence, char *proteinSequence, int *map, int *rdna, int *rpro)
 {
     char readingFrame[4] = "";
-    while (dnaSequence[*ldna] != '\0')
-    {
-        if (dnaSequence[*ldna + 1] != '\0' && dnaSequence[*ldna + 2] != '\0')
-        {
-            readingFrame[0] = dnaSequence[*ldna];
-            readingFrame[1] = dnaSequence[*ldna + 1];
-            readingFrame[2] = dnaSequence[*ldna + 2];
-            readingFrame[3] = '\0';
-            if (CheckCode(map[(int)proteinSequence[*lpro]], aminoacids, readingFrame))
-            {
-                (*lpro)++;
-            }
-            else
-            {
-                return 1;
-            }
-        }
-        (*ldna) += 3;
-    }
-    return 0;
-}
-int findExon2(char *dnaSequence, char *proteinSequence, char (*aminoacids)[6][3], int *map, int ldna, int lpro, int rdna, int rpro)
-{
-    char readingFrame[4] = "";
-    int i;
-    int k, l;
-    for (; ldna < rdna; ldna++)
-    {
-        i = ldna;
-        k = lpro;
-        l = rpro;
-        for (; k <= l; k++)
-        {
-            readingFrame[0] = dnaSequence[i];
-            readingFrame[1] = dnaSequence[i + 1];
-            readingFrame[2] = dnaSequence[i + 2];
-            readingFrame[3] = '\0';
-            if (!CheckCode(map[(int)proteinSequence[k]], aminoacids, readingFrame))
-                break;
-            i+=3;
-        }
-        if(k > l){
-            return ldna;
-        }
-    }
-    return 1;
-}
-
-int findExon3(char *dnaSequence, char *proteinSequence, char (*aminoacids)[6][3], int *map, int *rdna, int *rpro)
-{
-    char readingFrame[4] = "";
-    readingFrame[0] = dnaSequence[*rdna];
-    readingFrame[1] = dnaSequence[*rdna + 1];
-    readingFrame[2] = dnaSequence[*rdna + 2];
-    readingFrame[3] = '\0';
-    if (!CheckCode(map[(int)proteinSequence[(*rpro)--]], aminoacids, readingFrame))
-        return 0;
-    for (; *rpro > 0; (*rpro)--)
+    for (; *rpro >= 0; (*rpro)--)
     {
         (*rdna) -= 3;
         readingFrame[0] = dnaSequence[*rdna];
         readingFrame[1] = dnaSequence[*rdna + 1];
         readingFrame[2] = dnaSequence[*rdna + 2];
         readingFrame[3] = '\0';
-        if (!CheckCode(map[(int)proteinSequence[*rpro]], aminoacids, readingFrame))
+        if (!CheckCode(map[(int)proteinSequence[*rpro]], readingFrame))
+        {
+            (*rdna) += 2;
             return 1;
+        }
     }
-    return 0;
+    return -1;
 }
 
 void getInput(char *dnaSequence, char *proteinSequence, int *dnaLen, int *proteinLen)
@@ -197,12 +255,10 @@ void getInput(char *dnaSequence, char *proteinSequence, int *dnaLen, int *protei
         {
             if (count == 0)
             {
-                dnaSequence[++(*dnaLen)] = ch;
                 count++;
             }
             else if (count == 1)
             {
-                proteinSequence[++(*proteinLen)] = ch;
                 count++;
             }
         }
